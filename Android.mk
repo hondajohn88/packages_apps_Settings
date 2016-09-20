63288a5be2b77a794b241f505a7fb394313625ca
Mazda-- <mazdarider23@gmail.com>
2016-09-20 15:31:51 -0400

    Integrate TeslaCoil into Settings

--- Android.mk
+++ Android.mk
@@ -15,6 +15,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
         $(call all-java-files-under, src) \
+        $(call all-java-files-under, ../TeslaCoil/src) \
         src/com/android/settings/EventLogTags.logtags
 LOCAL_SRC_FILES += src/org/codeaurora/wfcservice/IWFCService.aidl \
                    src/org/codeaurora/wfcservice/IWFCServiceCB.aidl
@@ -24,6 +25,11 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v14/preference/res \
     frameworks/support/v7/appcompat/res \
     frameworks/support/v7/recyclerview/res
+
+LOCAL_RESOURCE_DIR += packages/apps/TeslaCoil/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+      --extra-packages com.gzr.teslacoil
 
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
