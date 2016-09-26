f8d2b5ecc4d88f0fbf43091cab491f5cd9e79062
beanstown106 <nbenis106@gmail.com>
2016-09-25 21:28:13 -0400

    Add Phh Superuser dashboard tile
    credit to @tsubus for his setting dashboard implementation
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -238,6 +238,7 @@ public class SettingsActivity extends SettingsDrawerActivity
     private static final String ACTION_TIMER_SWITCH = "qualcomm.intent.action.TIMER_SWITCH";
 
     private static final String SUPERSU_FRAGMENT = "com.android.settings.SuperSU";
+    private static final String SUPERUSER_FRAGMENT = "com.android.settings.SuperUser";
 
     private static final String LTE_4G_FRAGMENT = "com.android.settings.Lte4GEnableSetting";
     private static final String PROFILEMGR_MAIN_FRAGMENT = "com.android.settings.ProfileMgrMain";
@@ -1093,6 +1094,14 @@ public class SettingsActivity extends SettingsDrawerActivity
             finish();
             return null;
         }
+        if (SUPERUSER_FRAGMENT.equals(fragmentName)) {
+            Intent superuserIntent = new Intent();
+            superuserIntent.setClassName("me.phh.superuser", "com.koushikdutta.superuser.MainActivity");
+            startActivity(superuserIntent);
+
+            finish();
+            return null;
+        }
 
 
         if (SYSTEM_UPDATE.equals(fragmentName)) {
@@ -1271,6 +1280,16 @@ public class SettingsActivity extends SettingsDrawerActivity
         setTileEnabled(new ComponentName(packageName,
                         Settings.SuperSUActivity.class.getName()),
                 suSupported, isAdmin, pm);
+
+        // SuperUser
+        boolean phhSupported = false;
+        try {
+            phhSupported = (getPackageManager().getPackageInfo("me.phh.superuser", 0).versionCode > 0);
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+        setTileEnabled(new ComponentName(packageName,
+                        Settings.SuperUserActivity.class.getName()),
+                phhSupported, isAdmin, pm);
 
         // Reveal development-only quick settings tiles
         DevelopmentTiles.setTilesEnabled(this, showDev);
